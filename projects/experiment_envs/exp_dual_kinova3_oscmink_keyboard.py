341f87e0181f24f34e986fde447fd124b730d68b
C. Kong <ckong35@gatech.edu>
2026-01-14 21:16:26 -0500

    [checkpoint] fixed small path bug

--- projects/experiment_envs/exp_dual_kinova3_oscmink_keyboard.py
+++ projects/experiment_envs/exp_dual_kinova3_oscmink_keyboard.py
@@ -109,15 +109,15 @@ import os
 if __name__ == "__main__":
     # Get the path of robosuite
     repo_path = os.path.abspath(
-        os.path.join(os.path.abspath(__file__), os.pardir, os.pardir, os.pardir, os.pardir)
+        os.path.join(os.path.abspath(__file__), os.pardir, os.pardir, os.pardir)
     )
     # Mink path
     mink_controller_path = os.path.join(
-        repo_path, "SEW-Geometric-Teleop", "projects", "dual_kinova3_teleop", "controllers", "config", "robots", "dualkinova3_mink_ik.json"
+        repo_path, "projects", "dual_kinova3_teleop", "controllers", "config", "robots", "dualkinova3_mink_ik.json"
     )
     # OSC path
     osc_controller_path = os.path.join(
-        repo_path, "SEW-Geometric-Teleop", "projects", "dual_kinova3_teleop", "controllers", "config", "robots", "dualkinova3_osc.json"
+        repo_path, "projects", "dual_kinova3_teleop", "controllers", "config", "robots", "dualkinova3_osc.json"
     )
 
     parser = argparse.ArgumentParser()
